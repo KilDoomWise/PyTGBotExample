a5d01090097fa7accf9efcdeebbb39c1afa5e17c
KilDoom <117968565+KilDoomWise@users.noreply.github.com>
2025-06-25 23:54:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+print("ENV Loaded!")
+
+print("BOT TOKEN: " + os.environ("BOT_TOKEN"))
+
+bot = telebot.TeleBot(os.environ("BOT_TOKEN"), parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+print("Test bot is running!")
+
+bot.infinity_polling()
